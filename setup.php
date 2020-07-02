b18105d1fbe01c0f98a0ac41930e0dd39b0e3386
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-07-02 22:45:35 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -1,8 +1,3 @@
-<?php
-for($i = 0; $i < 3; $i++){
-  if($i = 3) $i = 0;
-}
-?>
 <!DOCTYPE HTML>
 <html>
   <head>
@@ -13,5 +8,18 @@ for($i = 0; $i < 3; $i++){
     <header style="width: 1600px; height: 800px;">
       <img src="image/logo.png" style="width: 1600px; height: 800px; align: center;" alt="Translator Logo">
     </header>
-    <p>We need to collect some infos<?= $i = 1 ? "." : "" ?><?= $i = 2 ? ".." : "" ?><?= $i = 3 ? "..." : "" ?></p>
+    <p id="collectInfos">We need to collect some infos</p>
+    <form method="POST">
+      <label for="databaseType">Database Type: </label>
+      <select id="databaseType" name="databaseType">
+        <option value="null" id="databaseTypeSelected" selected>-- Choose --</option>
+        <option value="mysql">MySQL/MariaDB</option>
+      </select><br>
+      <label for="databaseName">Database Name: </label><input type="text" name="databaseName" id="databaseName" placeholder="Database Name" value="<?= $_POST["databaseName"] ?>">
+      <label for="databaseUser">Database User: </label><input type="text" name="databaseUser" id="databaseUser" placeholder="Database User" value="<?= $_POST["databaseUser"] ?>">
+      <label for="databaseUserPassword">Database User Password: </label><input type="password" name="databaseUserPassword" id="databaseUserPassword" placeholder="Database User Password">
+      <label for="databaseTable">Database Table: </label><input type="text" name="databaseTable" id="databaseTable" placeholder="Database Table" value="<?= $_POST["databaseTable"] ?>">
+    </form>
+    <script src="js/script.js"></script>
+  </body>
 </html>
