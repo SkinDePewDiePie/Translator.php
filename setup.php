cf9afb26c676b52aecf62f60cae35d679e1c56dd
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-07-03 10:18:02 +0200

    Update setup.php

--- setup.php
+++ setup.php
@@ -6,36 +6,38 @@ function issetAndNotEmpty($variable){
 $error = "";
 $success = "";
 
-if(issetAndNotEmpty($_POST["databaseType"]) && issetAndNotEmpty($_POST["databaseHost"]) && issetAndNotEmpty($_POST["databaseName"]) && issetAndNotEmpty($_POST["databaseUser"]) && issetAndNotEmpty($_POST["databaseUserPassword"]) && issetAndNotEmpty($_POST["databaseTable"])){
-  $databaseType = htmlspecialchars(strip_tags($_POST["databaseType"])) != "null" ? htmlspecialchars(strip_tags($_POST["databaseType"])) : null;
-  $databaseHost = htmlspecialchars(strip_tags($_POST["databaseHost"]));
-  $databaseName = htmlspecialchars(strip_tags($_POST["databaseType"]));
-  $databaseUser = htmlspecialchars(strip_tags($_POST["databaseUser"]));
-  $databaseUserPassword = htmlspecialchars(strip_tags($_POST["databaseUserPassword"]));
-  $databaseTable = htmlspecialchars(strip_tags($_POST["databaseTable"]));
-  
-  $testDatabaseConnect = new PDO($databaseType.":host=".$databaseHost.";dbname=".$databaseName, $databaseUser, $databaseUserPassword);
-  
-  if($testDatabaseConnect){
-    $tableCreateRequest = $testDatabaseConnect->prepare("CREATE TABLE ".$databaseTable." IF NOT EXISTS ( textKey VARCHAR(255), translatedKey VARCHAR(255), language VARCHAR(255))");
-    if($tableCreateRequest->execute()){
-      $translatorTest = fopen(dirname(__FILE__)."/translatorTest.php", "a+");
-      fwrite($translatorTest, "<?php\n");
-      fwrite($translatorTest, "require(dirname(__FILE__).\"/Translator.php/TranslatorFunctions.php\");\n\n");
-      fwrite($translatorTest, "$translator = new \\Translator\\Base\\TranslatorFunctions(dirname(__FILE__));\n");
-      if(extension_enabled("geoip")) fwrite($translatorTest, "$translator->__automaticallyDetectLanguage();");
-      fwrite($translatorTest, "$translator->__connectToDatabase(\"".$databaseType."\", \"".$databaseHost."\", \"".$databaseName."\", \"".$databaseUser."\", \"".$databaseUserPassword."\", \"".$databaseTable."\")");
-      fclose($translatorTest);
-      
-      $success = "Created the translatorTest.php file located at: ".dirname(__FILE__)."/translatorTest.php !";
+if(isset($_POST["databaseConnect"])){
+  if(issetAndNotEmpty($_POST["databaseType"]) && issetAndNotEmpty($_POST["databaseHost"]) && issetAndNotEmpty($_POST["databaseName"]) && issetAndNotEmpty($_POST["databaseUser"]) && issetAndNotEmpty($_POST["databaseUserPassword"]) && issetAndNotEmpty($_POST["databaseTable"])){
+    $databaseType = htmlspecialchars(strip_tags($_POST["databaseType"])) != "null" ? htmlspecialchars(strip_tags($_POST["databaseType"])) : null;
+    $databaseHost = htmlspecialchars(strip_tags($_POST["databaseHost"]));
+    $databaseName = htmlspecialchars(strip_tags($_POST["databaseType"]));
+    $databaseUser = htmlspecialchars(strip_tags($_POST["databaseUser"]));
+    $databaseUserPassword = htmlspecialchars(strip_tags($_POST["databaseUserPassword"]));
+    $databaseTable = htmlspecialchars(strip_tags($_POST["databaseTable"]));
+
+    $testDatabaseConnect = new PDO($databaseType.":host=".$databaseHost.";dbname=".$databaseName, $databaseUser, $databaseUserPassword);
+
+    if($testDatabaseConnect){
+      $tableCreateRequest = $testDatabaseConnect->prepare("CREATE TABLE ".$databaseTable." IF NOT EXISTS ( textKey VARCHAR(255), translatedKey VARCHAR(255), language VARCHAR(255))");
+      if($tableCreateRequest->execute()){
+        $translatorTest = fopen(dirname(__FILE__)."/translatorTest.php", "a+");
+        fwrite($translatorTest, "<?php\n");
+        fwrite($translatorTest, "require(dirname(__FILE__).\"/Translator.php/TranslatorFunctions.php\");\n\n");
+        fwrite($translatorTest, "$translator = new \\Translator\\Base\\TranslatorFunctions(dirname(__FILE__));\n");
+        if(extension_enabled("geoip")) fwrite($translatorTest, "$translator->__automaticallyDetectLanguage();");
+        fwrite($translatorTest, "$translator->__connectToDatabase(\"".$databaseType."\", \"".$databaseHost."\", \"".$databaseName."\", \"".$databaseUser."\", \"".$databaseUserPassword."\", \"".$databaseTable."\")");
+        fclose($translatorTest);
+
+        $success = "Created the translatorTest.php file located at: ".dirname(__FILE__)."/translatorTest.php !";
+      } else{
+        $error = "Cannot create the table ! Check if the user have permission `CREATE TABLE`";
+      }
     } else{
-      $error = "Cannot create the table ! Check if the user have permission `CREATE TABLE`";
+      $error = "Cannot connect to the database !";
     }
   } else{
-    $error = "Cannot connect to the database !";
+    $error = "One/more fields must be completed !";
   }
-} else{
-  $error = "One/more fields must be completed !";
 }
 ?>
 <!DOCTYPE HTML>
@@ -60,6 +62,7 @@ if(issetAndNotEmpty($_POST["databaseType"]) && issetAndNotEmpty($_POST["database
       <label for="databaseUser">Database User: </label><input type="text" name="databaseUser" id="databaseUser" placeholder="Database User" value="<?= !isset($error) ? $_POST["databaseUser"] : "" ?>"><br>
       <label for="databaseUserPassword">Database User Password: </label><input type="password" name="databaseUserPassword" id="databaseUserPassword" placeholder="Database User Password"><br>
       <label for="databaseTable">Database Table: </label><input type="text" name="databaseTable" id="databaseTable" placeholder="Database Table" value="<?= !isset($error) ? $_POST["databaseTable"] : "" ?>">
+      <input type="submit" name="databaseConnect" value="Send.">
     </form>
     <p id="<?= $error ? "error" : "success" ?>"><?= $error ? $error : $success ?></p>
     <script src="js/script.js"></script>
