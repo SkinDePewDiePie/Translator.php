d4f69f07bc3b392a8e5e47069bad73bf8d305f4d
SkinDePewDiePie_ <35396314+SkinDePewDiePie@users.noreply.github.com>
2020-07-03 09:43:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 var collectInfos = document.getElementById("collectInfos");
+var databaseTypeSelected = document.getElementById("databaseTypeSelected");
 
 for(var i = 0; i < 3; i++){
-  if(i === 2
+  if(i === 0) collectInfos.innerHTML = `${collectInfos.innerHTML}.`;
+  if(i === 1) collectInfos.innerHTML = `${collectInfos.innerHTML}..`;
+  if(i === 2){
+    collectInfos.innerHTML = `${collectInfos.innerHTML}...`;
+    i = 0;
+  }
 }
+
